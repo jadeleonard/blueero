e959c0df4512dff9dc16f6a33d563b9900acdf6d
jadeleonard <lukabartolomeo9@gmail.com>
2023-12-27 03:11:47 -0800

    added eslint

--- src/Card.ts
+++ src/Card.ts
@@ -4,13 +4,18 @@ interface props {
   name:string
   href:string
   body:string
+  description:string
 }
-const Card : React.FC<props> = ({name,href,body}) => {
+const Card : React.FC<props> = ({name,href,body,description}) => {
   return (
     (
-      <div>
-
-    )
+      <div className='bg-zinc-200 rounded p-4 w-full h-auto'>
+      <p className='text-2xl'>{name}</p>
+      <p className='text-blue-400'>{href}</p>
+      <p className='text-slate-900'>{body}</p>
+      <p className='text-zinc-400 '>{description}</p>
+      </div>
+      )
     
   )
 }
