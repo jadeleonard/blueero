5d436216a7b40f4537519ca3d185d4f979bf55aa
jadeleonard <lukabartolomeo9@gmail.com>
2023-12-27 00:58:12 -0800

    Initial commit

---
+++ src/Card.ts
@@ -0,0 +1,18 @@
+import React from "react"
+
+interface props {
+  name:string
+  href:string
+  body:string
+}
+const Card : React.FC<props> = ({name,href,body}) => {
+  return (
+    (
+      <div>
+
+    )
+    
+  )
+}
+
+export default Card
---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+"use strict";
+
+import React from 'react'
+
+const Index = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Index
